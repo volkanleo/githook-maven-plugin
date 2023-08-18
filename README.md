ef351d28dd00d48d3304117290549e0d4a952cba
volkanleo <volkan.ozturk@leovegas.com>
2023-08-18 15:01:41 +0200

    Created git hook maven plugin

---
+++ README.md
@@ -0,0 +1,90 @@
+# Git Hook Install Maven Plugin
+The Git Hook Install Maven Plugin is a custom Maven plugin that allows you to automate the installation of Git hooks in your project's repository. Git hooks are scripts that Git executes before or after certain actions such as commits, merges, and pushes. This plugin simplifies the process of generating and installing these hooks by providing a seamless integration with Maven.
+
+## Why Git Hooks?
+Git hooks provide a way to automate tasks and checks in your repository. With hooks, you can ensure consistent code quality, enforce coding standards, run tests before committing, and perform various other actions automatically. By integrating Git hooks into your project, you streamline your development process and maintain higher code standards.
+
+## Protect your Version Control System
+It's always a good idea to check your changes before committing them: run unit tests, perform the build, etc. However, such check-lists may be easily overlooked, especially in big projects. To get rid of the human factor, they should be somehow forced and automated. The best way is to implement such verification on the project infrastructure level.
+
+## Features
+Automatically generates and installs Git hooks based on configuration provided in the Maven project.
+Supports both inline hooks with predefined scripts and resource hooks with external script files.
+Provides a default script that checks for updated dependencies and microservice versions before certain Git actions.
+
+## So why should I use this plugin?
+Because it deals with the problem of providing hook configuration to the repository, and automates their installation.
+
+## Usage
+The plugin provides the only goal "install". It's mapped on "initialize" phase by default. To use the default flow add these lines to the plugin definition:
+
+1- Add Plugin Configuration: In your project's pom.xml, add the plugin configuration under the build section:
+```
+<build>
+    <plugins>
+        <plugin>
+            <groupId>com.leovegas</groupId>
+            <artifactId>git-hook-install-maven-plugin</artifactId>
+            <version>1.0.0</version>
+            <executions>
+                <execution>
+                    <goals>
+                        <goal>install</goal>
+                    </goals>
+                </execution>
+            </executions>
+            <configuration>
+                <!-- Define hooks and resourceHooks here -->
+            </configuration>
+        </plugin>
+    </plugins>
+</build>
+```
+Configure Hooks: Define your hooks in the <hooks> and <resourceHooks> sections of the plugin configuration. Inline hooks have predefined scripts, while resource hooks link to external script files.
+```
+<hooks>
+  <hook-name/>
+  ...
+</hooks>
+```
+NOTE: The plugin rewrites hooks.
+
+## Usage Example
+```
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>com.leovegas</groupId>
+				<artifactId>githook-maven-plugin</artifactId>
+				<version>0.0.1-SNAPSHOT</version>
+				<executions>
+					<execution>
+						<goals>
+							<goal>install</goal>
+						</goals>
+						<configuration>
+							<hooks>
+								<pre-commit/>
+							</hooks>
+						</configuration>
+					</execution>
+				</executions>
+			</plugin>
+		</plugins>
+	</build>
+```
+
+
+External hook files can also been used :
+```
+...
+<configuration>
+    <hooks>
+        <resourceHooks>
+            <pre-push>hooks/pre-push.sh</pre-push>
+           <!-- Add more resource hooks as needed -->
+        </resourceHooks>
+    </hooks>
+</configuration>
+...
+```
---
+++ src/main/java/com/leovegas/GitHookInstallMojo.java
@@ -0,0 +1,140 @@
+package com.leovegas;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Map;
+
+import static java.nio.file.StandardOpenOption.*;
+
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+/**
+ * Maven Mojo for installing Git hooks in a project repository.
+ */
+@Mojo(name = "install", defaultPhase = LifecyclePhase.INITIALIZE)
+public final class GitHookInstallMojo extends AbstractMojo {
+
+    private static final String SHEBANG = "#!/bin/sh";
+    private static final Path HOOK_DIR_PATH = Paths.get(".git/hooks");
+
+    @Parameter(name = "hooks")
+    private Map<String, String> hooks;
+
+    @Parameter(name = "resourceHooks")
+    private Map<String, String> resourceHooks;
+
+    /**
+     * Executes the Git hook installation process.
+     *
+     * @throws MojoExecutionException If an execution error occurs.
+     * @throws MojoFailureException   If the plugin encounters a failure.
+     */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (!Files.exists(HOOK_DIR_PATH)) {
+            throw new MojoExecutionException("not a git repository");
+        }
+        this.generateInlineHooks();
+        this.generateResourceHooks();
+    }
+
+    /**
+     * Generates inline Git hooks using predefined scripts.
+     *
+     * @throws MojoExecutionException If an execution error occurs.
+     */
+    private void generateInlineHooks() throws MojoExecutionException {
+        if (hooks == null) {
+            return;
+        }
+        for (Map.Entry<String, String> hook : hooks.entrySet()) {
+            String hookName = hook.getKey();
+            getLog().info("Generating " + hookName + " from Maven conf");
+            generateHookFile(hookName, SHEBANG + '\n' + getDefaultScript());
+        }
+    }
+
+    /**
+     * Generates resource-based Git hooks by reading external script files.
+     *
+     * @throws MojoExecutionException If an execution error occurs.
+     */
+    private void generateResourceHooks() throws MojoExecutionException {
+        if (resourceHooks == null) {
+            return;
+        }
+        for (Map.Entry<String, String> hook : resourceHooks.entrySet()) {
+            String hookName = hook.getKey();
+
+            Path hookFilePath = Paths.get(hook.getValue());
+            Path local = Paths.get("");
+
+            if (!hookFilePath.toAbsolutePath().startsWith(local.toAbsolutePath())) {
+                throw new MojoExecutionException("only file inside the project can be used to generate git hooks");
+            }
+            try {
+                getLog().info("Generating " + hookName + " from " + hookFilePath.toString());
+                generateHookFile(hookName, Files.lines(hookFilePath).collect(Collectors.joining("\n")));
+            } catch (IOException e) {
+                throw new MojoExecutionException("could not access hook resource : " + hookFilePath, e);
+            }
+        }
+    }
+
+    /**
+     * Generates a hook file with the provided name and content.
+     *
+     * @param hookName        Name of the hook.
+     * @param asStringScript  Script content as a string.
+     * @throws MojoExecutionException If an execution error occurs.
+     */
+    private void generateHookFile(String hookName, String asStringScript) throws MojoExecutionException {
+        try {
+            Path path = HOOK_DIR_PATH.resolve(hookName);
+            Files.write(
+                    path,
+                    asStringScript.getBytes(),
+                    CREATE, TRUNCATE_EXISTING
+            );
+            Files.setPosixFilePermissions(
+                    path,
+                    new HashSet<>(Arrays.asList(
+                            PosixFilePermission.OWNER_EXECUTE,
+                            PosixFilePermission.OWNER_READ,
+                            PosixFilePermission.OWNER_WRITE
+                    ))
+            );
+        } catch (IOException e) {
+            throw new MojoExecutionException("could not write hook with name : " + hookName, e);
+        }
+    }
+
+    /**
+     * Returns the default inline script for checking dependency updates.
+     *
+     * @return The default inline script.
+     */
+    private String getDefaultScript() {
+        return "# Change directory to the project's root\n" +
+                "cd \"$(git rev-parse --show-toplevel)\"\n\n" +
+                "# Check for updated dependencies and microservice versions\n" +
+                "dependency_updates=$(mvn versions:display-property-updates versions:display-parent-updates -DgenerateBackupPoms=false \\\n" +
+                "    | grep '\\->' \\\n" +
+                "    | awk -F ' ' '{if ($2 != $4) print $0}')\n\n" +
+                "# Check if dependency updates are available\n" +
+                "if [[ -n \"$dependency_updates\" ]]; then\n" +
+                "    echo \"WARNING: The following dependencies or microservices have updates available:\"\n" +
+                "    echo \"$dependency_updates\"\n" +
+                "fi";
+    }
+}
